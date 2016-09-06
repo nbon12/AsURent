01930dece495425149aa2c14a267cf859beb3de5
nbon <bonillani@appstate.edu>
2016-09-06 22:52:14 +0000

    initial commit

---
+++ app/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    /**
+     * Get all tasks for the user.
+     */
+     public function tasks()
+     {
+         return $this->hasMany(Task::class); 
+     }
+     
+     /**
+      * Get all contracts for the user.
+      */
+      public function contracts()
+      {
+          return $this->hasMany(Contract::class);
+      }
+    
+}
