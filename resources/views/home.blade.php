4bfd7a2169897da9e693e5a7fc256ba0b86d8f93
nbon <bonillani@appstate.edu>
2016-10-04 22:28:01 +0000

    new commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    testing change!
                 </div>
             </div>
             <form action="{{route('lalatask')}}">
