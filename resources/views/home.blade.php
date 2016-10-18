b4e23c00bc0581f24499b148634e4a0660da0fa1
nbon <bonillani@appstate.edu>
2016-10-18 21:12:18 +0000

    linked js and css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,12 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        
+        
+        <div class="col-md-10 col-md-offset-1 testchange">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
+                <div class="panel-body container-fluid testchange">
                     testing change!
                 </div>
             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,10 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="{{asset('css/css.css')}}"/>
+    
+    <!--java script -->
+     <script type="text/javascript" src="{{asset('js/site.js')}}"></script>
 
     <style>
         body {
