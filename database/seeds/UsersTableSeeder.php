682f2d2152a1bf1fbd6575360ff98b0a2f89fe6f
nbon <bonillani@appstate.edu>
2016-11-15 20:59:41 +0000

    sync to master

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        Stripe::setApiKey(env('STRIPE_SECRET'));
         $customer = \Stripe\Customer::create(array(
           "description" => "Seeded Customer 1 for AsURent",
           "source" => null // will be replaced with btok...
@@ -19,7 +20,7 @@ class UsersTableSeeder extends Seeder
                 'name' => 'happy',
                 'email' => str_random(10).'@gmail.com',
                 'password' => bcrypt('squalid'),
-                'stripe_customer_id' => $customer->id;
+                'stripe_customer_id' => $customer->id
         ]);
         $customer = \Stripe\Customer::create(array(
           "description" => "Seeded Customer 2 for AsURent",
@@ -29,7 +30,7 @@ class UsersTableSeeder extends Seeder
                 'name' => 'nick',
                 'email' => 'nick@nick.nick',
                 'password' => bcrypt(env('NICK_PASSWORD', 'password')),
-                'stripe_customer_id' => $customer->id;
+                'stripe_customer_id' => $customer->id
         ]);
         $customer = \Stripe\Customer::create(array(
           "description" => "Seeded Customer 3 for AsURent",
@@ -39,7 +40,7 @@ class UsersTableSeeder extends Seeder
                 'name' => 'tenantA',
                 'email' => 'tenantA@tenant.com',
                 'password' => bcrypt(env('NICK_PASSWORD', 'password')),
-                'stripe_customer_id' => $customer->id;
+                'stripe_customer_id' => $customer->id
                 
         ]);
         
